828b41ba8fb1e831e5d6a942996fa8dd6644170b
LuisMauricioCastro <98352322+LuisMauricioCastro@users.noreply.github.com>
2022-02-14 15:07:05 -0700

    Update and rename Sensor touch con delay blink to Sensor touch con delay blink.cpp

--- Sensor touch con delay blink.cpp
+++ Sensor touch con delay blink.cpp
@@ -6,6 +6,18 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 }
 void loop (){
+
+  if(delay_Hs <200){
+    delay_Hs=5000;
+  }
+  if(delay_Hs >5000){
+    delay_Hs=200;
+  }
+  if (touchRead(T4)< 60) {
+    delay_Hs -= 200;
+  } else if (touchRead(T3) < 60) {
+    delay_Hs += 200;
+  }
   cout << "Valor G15=" << touchRead(T3) << endl; 
   cout << "Valor G14=" << touchRead(T4) << endl; 
   cout << "Valor de delay =" << delay_Hs << endl;
@@ -13,9 +25,4 @@ void loop (){
   delay (delay_Hs);
   digitalWrite(LED_BUILTIN, LOW); 
   delay (delay_Hs);
-  if (touchRead(T4)< 70) {
-    delay_Hs -= 200;
-  } else if (touchRead(T3) < 60) {
-    delay_Hs += 200;
-  }
 }
